500292c9e6b391c85fae224ab20823009362f0c8
rijwan sakib <71920497+rijwansakib@users.noreply.github.com>
2023-03-02 17:30:44 +0600

    Authorization

--- routes/brand.route.js
+++ routes/brand.route.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router= express.Router();
 const brandController=require('../controllers/brand.controller')
+const authorization = require("../middleWare/authorization");
+const verifyToken = require("../middleware/verifyToken");
 
 router.route('/')
-    .post(brandController.createBrand)
+    .post(verifyToken, authorization("admin", "store-manage"),brandController.createBrand)
     .get(brandController.getBrand)
 
 module.exports=router
